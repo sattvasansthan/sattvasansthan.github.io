f7f86fd344226bb3c7550d659dbe98527ab3d672
Prakash Solanki <prakashs.mca22@mbm.ac.in>
2024-08-12 08:19:43 +0530

    Gallary chnages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,68 +1,68 @@
 
-var thankam = angular.module('thankam',['ui.router']);
+var thankam = angular.module('thankam', ['ui.router']);
 var url = 'http://localhost/learning/le-admin/';
-thankam.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider){
-    $urlRouterProvider.otherwise('/');
+thankam.config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider, $stateProvider) {
+  $urlRouterProvider.otherwise('/');
 
-    $stateProvider
-        .state('home',{
-            url:'/',
-            templateUrl: 'template/home.html'
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'template/home.html'
 
-        })
-     $stateProvider
-        .state('about',{
-            url:'/about',
-            templateUrl: 'template/about.html'
+    })
+  $stateProvider
+    .state('about', {
+      url: '/about',
+      templateUrl: 'template/about.html'
 
-        })
-      $stateProvider
-        .state('support',{
-            url:'/support',
-            templateUrl: 'template/support.html'
+    })
+  $stateProvider
+    .state('support', {
+      url: '/support',
+      templateUrl: 'template/support.html'
 
-        })
-       $stateProvider
-        .state('gallery',{
-            url:'/gallery',
-            templateUrl: 'template/gallery.html'
+    })
+  $stateProvider
+    .state('gallery', {
+      url: '/gallery',
+      templateUrl: 'template/gallery.html'
 
-        })
-       $stateProvider
-        .state('testimony',{
-            url:'/testimony',
-            templateUrl: 'template/testimony.html'
+    })
+  $stateProvider
+    .state('testimony', {
+      url: '/testimony',
+      templateUrl: 'template/testimony.html'
 
-        })
-       $stateProvider
-        .state('contact',{
-            url:'/contact',
-            templateUrl: 'template/contact.html'
+    })
+  $stateProvider
+    .state('contact', {
+      url: '/contact',
+      templateUrl: 'template/contact.html'
 
-        })
-       
-       $stateProvider
-        .state('news',{
-            url:'/news',
-            templateUrl: 'template/news.html'
+    })
 
-        })
-       $stateProvider
-        .state('payment',{
-            url:'/payment',
-            templateUrl: 'template/payment.html'
+  $stateProvider
+    .state('news', {
+      url: '/news',
+      templateUrl: 'template/news.html'
 
-        })
+    })
+  $stateProvider
+    .state('payment', {
+      url: '/payment',
+      templateUrl: 'template/payment.html'
+
+    })
 }])
 
 
 
-thankam.controller('homeCtrl', function($scope, $http){
-	
+thankam.controller('homeCtrl', function ($scope, $http) {
+
 })
 
 
-$(window).load(function() {
+$(window).load(function () {
   $('.flexslider').flexslider({
     animation: "slide"
   });
@@ -70,15 +70,23 @@ $(window).load(function() {
 
 
 
-function changeSelect(i){
-  $(".mom").removeClass('sel'+i);
-  $("#mm"+i).addClass('sel'+i);
+function changeSelect(i) {
+  $(".mom").removeClass('sel' + i);
+  $("#mm" + i).addClass('sel' + i);
 }
-  function showSM(){
-    $("#sm-menu").show(500);
-  }
+function showSM() {
+  $("#sm-menu").show(500);
+}
 
 
-  function hideSM(){
-    $("#sm-menu").hide();
-  }
+function hideSM() {
+  $("#sm-menu").hide();
+}
+
+$(document).ready(function () {
+  $(".fancybox").fancybox({
+    openEffect: "none",
+    closeEffect: "none"
+  });
+});
+
