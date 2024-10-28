b59f60d0147f05f25f8c6c1d89faaf3030c9a539
Nakesh Aswani <nakesh270704@gmail.com>
2024-10-28 12:33:23 +0530

    modified

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,25 +55,11 @@ thankam.config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProv
     })
 }])
 
-
-
-thankam.controller('homeCtrl', function ($scope, $http) {
-
-})
-
-
-$(window).load(function () {
-  $('.flexslider').flexslider({
-    animation: "slide"
-  });
-});
-
-
-
 function changeSelect(i) {
   $(".mom").removeClass('sel' + i);
   $("#mm" + i).addClass('sel' + i);
 }
+
 function showSM() {
   $("#sm-menu").show(500);
 }
@@ -84,9 +70,96 @@ function hideSM() {
 }
 
 $(document).ready(function () {
-  $(".fancybox").fancybox({
-    openEffect: "none",
-    closeEffect: "none"
+  var owl = $("#updatesSlider");
+  owl.owlCarousel({
+    margin: 20,
+    loop: true,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      200: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      992: {
+        items: 4
+      }
+    }
   });
 });
 
+$(document).ready(function () {
+  var owl = $("#gallerySlider1, #gallerySlider2, #gallerySlider3");
+  owl.owlCarousel({
+    margin: 20,
+    loop: true,
+    nav: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      200: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      992: {
+        items: 3
+      }
+    }
+  });
+});
+
+$('[data-fancybox="updates"]').fancybox({
+  buttons: [
+    "slideShow",
+    "thumbs",
+    "zoom",
+    "fullScreen",
+    "share",
+    "close"
+  ],
+  loop: true,
+  protect: true
+});
+
+function fixHeader() {
+  var header = document.querySelector('#header');
+  var headerPlaceholder = document.querySelector('#headerPlaceholder');
+  var sticky = header.clientHeight;
+  console.log(sticky)
+  if (window.scrollY > sticky) {
+    header.classList.add("position-fixed", "top-0", "start-0");
+    headerPlaceholder.classList.remove("d-none")
+  } else {
+    header.classList.remove("position-fixed", "top-0", "start-0");
+    headerPlaceholder.classList.add("d-none")
+  }
+}
+
+document.querySelectorAll('.volFormBlock input, .volFormBlock textarea').forEach(input => {
+  const label = input.nextElementSibling;
+
+  // Move label up if input is focused or has value
+  input.addEventListener('focus', () => {
+    label.classList.add('active');
+  });
+
+  input.addEventListener('blur', () => {
+    if (!input.value) {
+      label.classList.remove('active');
+    }
+  });
+
+  // Check if there is already a value (when page reloads)
+  if (input.value) {
+    label.classList.add('active');
+  }
+});
